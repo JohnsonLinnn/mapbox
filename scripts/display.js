9315b745a5dd5ecba5add938a544ce30fe11705b
林展丞 <105207346@mail2.nccu.tw>
2020-07-13 14:40:14 +0800

    basic setup finished

---
+++ scripts/display.js
@@ -0,0 +1,7 @@
+console.log("link working")
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoiam9obnNvbjAwMCIsImEiOiJja2NiaHk0OGYwMWx1MnJxbm5sd3RpODk0In0.21o4OgxKm1NVUOoX-ktxyQ';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11'
+});
